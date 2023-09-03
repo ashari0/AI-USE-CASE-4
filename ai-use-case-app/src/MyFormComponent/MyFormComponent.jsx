11cd15f81e987942af68b9af51dbc909a3d1f527
Ashari <ashari@softserveinc.com>
2023-09-04 02:27:59 +0300

    UC#7 Create react unit tests

--- ai-use-case-app/src/MyFormComponent/MyFormComponent.jsx
+++ ai-use-case-app/src/MyFormComponent/MyFormComponent.jsx
@@ -1,55 +1,58 @@
-import React, { useState, useEffect } from 'react';
- 
+import React, { useState, useEffect } from "react";
+
 function MyFormComponent() {
-    const [form, setForm] = useState({
-        name: '',
-        email: '',
-        agreeTerms: false,
-        gender: '',
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    agreeTerms: false,
+    gender: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
+
+  const sendDataToMockApi = async (formData) => {
+    try {
+      const response = await fetch("MOCK_API", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
       });
-      
-      const [errors, setErrors] = useState({});
-      
-      const handleChange = e => {
-        const { name, value, type, checked } = e.target;
-        setForm(prevForm => ({
-          ...prevForm,
-          [name]: type === 'checkbox' ? checked : value
-        }));
-      };
-      
-      const sendDataToMockApi = async (formData) => {
-        try {
-          const response = await fetch('MOCK_API', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-          });
-    
-          if (!response.ok) {
-            console.error("Error while sending form data:", response.statusText);
-          }
-        } catch (error) {
-          console.error("Error:", error);
-        }
-      };
-    
-      const handleSubmit = e => {
-        e.preventDefault();
-        sendDataToMockApi(form);
-      };
-      
-      useEffect(() => {
-        let newErrors = {};
-        if (form.name.length < 3) newErrors.name = 'Name must be at least 3 characters.';
-        if (!form.email.includes('@')) newErrors.email = 'Email must be valid.';
-        if (!form.agreeTerms) newErrors.agreeTerms = 'You must agree to the terms.';
-        if (!form.gender) newErrors.gender = 'You must select a gender.';
-        setErrors(newErrors);
-      }, [form]);
-  
+
+      if (!response.ok) {
+        console.error("Error while sending form data:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!Object.keys(errors).length) {
+      sendDataToMockApi(form);
+    }
+  };
+
+  useEffect(() => {
+    let newErrors = {};
+    if (form.name.length < 3)
+      newErrors.name = "Name must be at least 3 characters.";
+    if (!form.email.includes("@")) newErrors.email = "Email must be valid.";
+    if (!form.agreeTerms) newErrors.agreeTerms = "You must agree to the terms.";
+    if (!form.gender) newErrors.gender = "You must select a gender.";
+    setErrors(newErrors);
+  }, [form]);
+
   return (
     <form onSubmit={handleSubmit}>
       <input
@@ -60,7 +63,6 @@ function MyFormComponent() {
         placeholder="Name"
       />
       {errors.name && <p>{errors.name}</p>}
- 
       <input
         type="email"
         name="email"
@@ -69,35 +71,34 @@ function MyFormComponent() {
         placeholder="Email"
       />
       {errors.email && <p>{errors.email}</p>}
- 
       <input
         type="checkbox"
         name="agreeTerms"
         checked={form.agreeTerms}
         onChange={handleChange}
-      /> Agree to Terms
+      />{" "}
+      Agree to Terms
       {errors.agreeTerms && <p>{errors.agreeTerms}</p>}
- 
       <input
         type="radio"
         name="gender"
         value="male"
         checked={form.gender === "male"}
         onChange={handleChange}
-      /> Male
- 
+      />{" "}
+      Male
       <input
         type="radio"
         name="gender"
         value="female"
         checked={form.gender === "female"}
         onChange={handleChange}
-      /> Female
+      />{" "}
+      Female
       {errors.gender && <p>{errors.gender}</p>}
- 
       <button type="submit">Submit</button>
     </form>
   );
 }
- 
+
 export default MyFormComponent;
--- ai-use-case-app/src/MyFormComponent/__tests__/MyFormComponent.test.js
+++ ai-use-case-app/src/MyFormComponent/__tests__/MyFormComponent.test.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import MyFormComponent from "../MyFormComponent";
 
 describe("MyFormComponent", () => {
-  // Just removes unnecessary console error output (unnecessary for this case)
-  console.error = jest.fn();
-
-  const setup = (name, email, isAgreeWithTerms, gender) => {
+  const setup = async (name, email, isAgreeWithTerms, gender) => {
     render(<MyFormComponent />);
-    global.fetch = jest.fn(() => Promise.resolve({ ok: true }));
-
-    userEvent.type(screen.getByPlaceholderText("Name"), name);
-    userEvent.type(screen.getByPlaceholderText("Email"), email);
+    fireEvent.change(screen.getByPlaceholderText("Name"), {
+      target: { value: name },
+    });
+    fireEvent.change(screen.getByPlaceholderText("Email"), {
+      target: { value: email },
+    });
     if (isAgreeWithTerms) {
       userEvent.click(screen.getByRole("checkbox"));
     }
@@ -21,79 +20,95 @@ describe("MyFormComponent", () => {
     }
   };
 
+  const mockFetch = jest.fn(() =>
+    Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve({ ok: true }),
+    })
+  );
+
+  beforeEach(() => {
+    window.fetch = mockFetch;
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
 
   test("Submit the form with all fields filled in correctly", async () => {
-    setup("Arsen Sharifov", "arsen.sharifov@gmail.com", true, "male");
+    await setup("Arsen Sharifov", "arsen.sharifov@gmail.com", true, "male");
+    await waitFor(() => {
+      expect(screen.getByPlaceholderText("Name").value).toBe("Arsen Sharifov");
+    });
     userEvent.click(screen.getByText("Submit"));
-    await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(mockFetch).toHaveBeenCalledTimes(1));
   });
 
   test("Submit the form with a very long valid name", async () => {
     setup(
-      "A".repeat(250) + " Sharifov",
+      "A".repeat(150) + " Sharifov",
       "arsen.sharifov@gmail.com",
       true,
       "male"
     );
     userEvent.click(screen.getByText("Submit"));
-    await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(mockFetch).toHaveBeenCalledTimes(1));
   });
 
   test("Submit the form with a complex email address", async () => {
     setup("Arsen Sharifov", "test.name+alias@example.co.uk", true, "male");
     userEvent.click(screen.getByText("Submit"));
-    await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(mockFetch).toHaveBeenCalledTimes(1));
   });
 
   test("Change the gender from male to female and submit the form", async () => {
     setup("Arsen Sharifov", "test.name+alias@example.co.uk", true, "male");
     userEvent.click(screen.getByDisplayValue("female"));
     userEvent.click(screen.getByText("Submit"));
-    await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(mockFetch).toHaveBeenCalledTimes(1));
   });
 
   test("Re-submit the form after an initial successful submission", async () => {
-    setup();
+    setup("Arsen Sharifov", "arsen.sharifov@gmail.com", true, "male");
     userEvent.click(screen.getByText("Submit"));
-    await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(mockFetch).toHaveBeenCalledTimes(1));
 
-    userEvent.type(screen.getByPlaceholderText("Name"), "Sharifov Arsen");
-    userEvent.type(
-      screen.getByPlaceholderText("Email"),
-      "sharifov.Arsen@example.com"
-    );
+    fireEvent.change(screen.getByPlaceholderText("Name"), {
+      target: { value: "New Name" },
+    });
+    fireEvent.change(screen.getByPlaceholderText("Email"), {
+      target: { value: "new.email@example.com" },
+    });
+    userEvent.click(screen.getByRole("checkbox"));
     userEvent.click(screen.getByRole("checkbox"));
     userEvent.click(screen.getByDisplayValue("male"));
 
     userEvent.click(screen.getByText("Submit"));
-    await waitFor(() => expect(fetch).toHaveBeenCalledTimes(2));
+    await waitFor(() => expect(mockFetch).toHaveBeenCalledTimes(2));
   });
 
   test("Submit the form with the 'Name' field left blank", async () => {
     setup("", "test.name+alias@example.co.uk", true, "male");
-    await waitFor(() => expect(fetch).not.toHaveBeenCalled());
+    await waitFor(() => expect(mockFetch).not.toHaveBeenCalled());
   });
 
   test("Submit the form with an invalid email address", async () => {
-    setup("Arsen Sharifov", "test.name", true, "male");
-    await waitFor(() => expect(fetch).not.toHaveBeenCalled());
+    setup("Arsen Sharifov", "sharifov.Arsen", true, "male");
+    await waitFor(() => expect(mockFetch).not.toHaveBeenCalled());
   });
 
   test("Submit the form without checking the 'Agree to Terms' checkbox", async () => {
     setup("Arsen Sharifov", "sharifov.Arsen@example.com", false, "male");
-    await waitFor(() => expect(fetch).not.toHaveBeenCalled());
+    await waitFor(() => expect(mockFetch).not.toHaveBeenCalled());
   });
 
   test("Submit the form without selecting a gender", async () => {
     setup("Arsen Sharifov", "sharifov.Arsen@example.com", true, "");
-    await waitFor(() => expect(fetch).not.toHaveBeenCalled());
+    await waitFor(() => expect(mockFetch).not.toHaveBeenCalled());
   });
 
   test("Submit the form with a name that is less than 3 characters long", async () => {
     setup("Ar", "sharifov.Arsen@example.com", true, "male");
-    await waitFor(() => expect(fetch).not.toHaveBeenCalled());
+    await waitFor(() => expect(mockFetch).not.toHaveBeenCalled());
   });
 });
