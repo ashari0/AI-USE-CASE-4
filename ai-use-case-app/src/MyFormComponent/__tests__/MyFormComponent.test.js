9476b3c77467c7d158977c692a3d84b5876498cc
Ashari <ashari@softserveinc.com>
2023-09-04 00:13:45 +0300

    UC#7 Create documentation

--- ai-use-case-app/src/MyFormComponent/__tests__/MyFormComponent.test.js
+++ ai-use-case-app/src/MyFormComponent/__tests__/MyFormComponent.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import MyFormComponent from "./MyFormComponent";
+import MyFormComponent from "../MyFormComponent";
 
 describe("MyFormComponent", () => {
   // Just removes unnecessary console error output (unnecessary for this case)
